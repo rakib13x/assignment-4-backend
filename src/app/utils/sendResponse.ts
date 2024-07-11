b46e6905382677c3f0f58ed8c7f13ebf8339b925
rakib13x <rakib13x@gmail.com>
2024-07-11 07:04:02 +0600

    added sendResponse for sending reusable response from every  api

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express';
+
+type TResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  data: T;
+};
+
+const sendResponse = <T>(res: Response, response: TResponse<T>): void => {
+  const { statusCode, success, message, data } = response;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const responseBody: any = {
+    success,
+    message,
+    data,
+  };
+
+  res.status(statusCode).json(responseBody);
+};
+
+export default sendResponse;
