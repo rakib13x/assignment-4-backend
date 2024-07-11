35068be53f248d68a12436bba446d53dbde0915f
rakib13x <rakib13x@gmail.com>
2024-07-11 08:21:59 +0600

    added getSingleProduct route

--- src/app/routes/route/product.route.ts
+++ src/app/routes/route/product.route.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/', ProductControllers.createProduct);
 router.get('/allProducts', ProductControllers.createProduct);
+router.get('/:productId', ProductControllers.getSingleProduct);
 
 export const ProductRoutes = router;
